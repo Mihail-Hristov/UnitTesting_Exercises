a3488fe70cfc3e3544d3454c4acc12fe40b7d356
mis6us <mis6us@icloud.com>
2020-11-27 23:47:31 +0200

    Added more tests

--- src/test/java/p01_Database/DatabaseTest.java
+++ src/test/java/p01_Database/DatabaseTest.java
@@ -7,27 +7,42 @@ import javax.naming.OperationNotSupportedException;
 
 public class DatabaseTest {
 
-    @Test(expected = OperationNotSupportedException.class)
-    public void testToCreateDatabaseWithMoreThan16IntegersLong() throws OperationNotSupportedException {
-        Database database = new Database(13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,1,31,3,13);
-    }
-
-    @Test(expected = OperationNotSupportedException.class)
-    public void testToCreateDatabaseWithLessThan16IntegersLong() throws OperationNotSupportedException {
-        Database database = new Database(13,13);
-    }
-
-    @Test(expected = OperationNotSupportedException.class)
-    public void testAddCommandWithNullPassedElement() throws OperationNotSupportedException {
-        Database database = new Database(13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13);
-        database.add(15);
-        database.add(15);
-        database.add(15);
-        database.add(15);
-        database.add(15);
-
-        for (int i = 16; i < 21; i++) {
-            Assert.assertEquals(database.getElements()[i], database.getIndex(), 0);
+    @Test
+    public void testDatabaseConstructorIsWorkCorrectly() throws OperationNotSupportedException {
+        Integer[] numbers = {34, 52, 25, 13, 10};
+        Database database = new Database(numbers);
+        Assert.assertEquals(5, database.getElements().length, 0);
+        for (int i = 0; i < database.getElements().length; i++) {
+            Assert.assertEquals(numbers[i], database.getElements()[i], 0);
         }
     }
+
+    @Test(expected = OperationNotSupportedException.class)
+    public void testToCreateDatabaseWithLessThan1IntegersLong() throws OperationNotSupportedException {
+        Integer[] numbers = new Integer[0];
+        Database database = new Database(numbers);
+    }
+
+    @Test(expected = OperationNotSupportedException.class)
+    public void testToCreateDatabaseWithMoreThan16IntegersLong() throws OperationNotSupportedException {
+        Integer[] numbers = new Integer[17];
+        Database database = new Database(numbers);
+    }
+
+    @Test(expected = OperationNotSupportedException.class)
+    public void testAddCommandWithNull() throws OperationNotSupportedException {
+      Integer[] numbers = {34, 2, 53};
+      Database database = new Database(numbers);
+      database.add(null);
+    }
+
+    @Test
+    public void testAddCommandForAddingElementOnNextFreeIndex() throws OperationNotSupportedException {
+        Integer[] numbers = {34, 2, 53};
+        Database database = new Database(numbers);
+        database.add(55);
+        int searchingIndex = numbers.length;
+        Assert.assertEquals(searchingIndex, database.getElements().length - 1, 0);
+    }
+
 }
